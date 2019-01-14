788fee2a6474f17d007f75048c489bf9adb0ad18
QuentinTHP <quentin.clere.pro@gmail.com>
2019-01-14 16:48:33 +0100

    Mise à jour fin d'aprem

--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -3,23 +3,43 @@ def who_is_bigger(x, y, z)
   tab = [ x, y, z]
   if tab.include?(nil)
     return "nil detected"
-    else
-  return tab.max
-    end
+  elsif x == tab.max 
+    "a is bigger"
+  elsif y == tab.max
+    "b is bigger"
+  elsif z == tab.max
+    "c is bigger"
+  end
 end
 
 
-def crazy_stuff_on_strings()
-
+def reverse_upcase_noLTA(string)
+string = string.reverse.upcase
+string.delete("LTA")
 end 
 
 
-def finder()
-
+def array_42(array)
+    array = array.each {|x|}
+    array.include?(42)
 end 
 
 
-def crazy_stuff_on_arrays()
+def magic_array(array2)
+      # The magic_array function takes an array of number or an array of
+  # array of number as parameter and return the same array :
+  # - flattened (i.e. no more arrays in array)
+  # - sorted
+  # - with each number multiplicated by 2
+  # - with each multiple of 3 removed
+  # - with each number duplicate removed (any number should appear only once)
+  # - sorted
+  # BONUS : You can do this in one line less than 55 chars
+array2 = array2.flatten.sort
+array2 = array2.map {|x| x*2}
+array2 = array2.reject{|total| total % 3 == 0}
+array2 = array2.uniq 
+array2 = array2.sort
 
 end 
 
