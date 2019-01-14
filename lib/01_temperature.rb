249b0e48d006a60868e3801318fc17ea84a5956d
QuentinTHP <quentin.clere.pro@gmail.com>
2019-01-14 13:38:02 +0100

    okokok

---
+++ lib/01_temperature.rb
@@ -0,0 +1,7 @@
+def ftoc(f)
+    return ((f - 32)/1.8).round
+end 
+
+def ctof(c)
+    return  ((c * 1.8)+32)
+end
---
+++ lib/02_calculator.rb
@@ -0,0 +1,32 @@
+def add(a,b)
+    return (a+b)
+end
+
+
+def subtract(a,b)
+    return (a-b)
+end
+
+
+def sum (array)
+    array.sum
+end 
+
+
+def multiply (a,b)
+    return (a*b)
+end
+
+
+def power (a,b)
+    return (a**b)
+end
+
+
+def factorial(a)
+    if a == 0 || a < 0 
+        =0 
+return 1.upto(a).count do |i|
+end
+
+end
